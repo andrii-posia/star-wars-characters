2b79ca5a51ee22dd6695e77c43eb33d80a0acc8c
andrii-posia <andrii.posia@gmail.com>
2019-03-13 12:23:13 +0200

    create empty components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CharacterComponent } from './components/character/character.component';
+import { FilterComponent } from './components/filter/filter.component';
+import { CharacterListComponent } from './components/character-list/character-list.component';
+import { HttpService } from './services/http.service';
+import { CharacterService } from './services/character.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharacterComponent,
+    FilterComponent,
+    CharacterListComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    HttpService,
+    CharacterService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
